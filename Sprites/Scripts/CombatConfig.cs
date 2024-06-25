3244fcf266f690a1f47ff9617c2011f8571dc16c
Matvii <100190295+Mathssssssss@users.noreply.github.com>
2024-06-25 16:39:31 +0100

    fixing bag

--- Sprites/Scripts/CombatConfig.cs
+++ Sprites/Scripts/CombatConfig.cs
@@ -9,10 +9,10 @@ public class CombatConfig : MonoBehaviour
     [SerializeField] private int Damage;
     [SerializeField] private int Heal;
 
-    public int MaxHealth1 { get => MaxHealth;}
-    public int CurrentHealth { get => CurrentHealth; set => CurrentHealth = value; }
-    public int Damage { get => Damage;}
-    public int Heal { get => Heal;}
+    public int maxHealth { get => MaxHealth;}
+    public int currentHealth { get => CurrentHealth; set => CurrentHealth = value; }
+    public int damage { get => Damage;}
+    public int heal { get => Heal;}
 
 
 }
