460258330261bb33b56464ca43346ef9bcf17d33
Matvii <100190295+Mathssssssss@users.noreply.github.com>
2024-06-25 16:44:07 +0100

    health bar updating

--- Sprites/Scripts/GameMaster.cs
+++ Sprites/Scripts/GameMaster.cs
@@ -6,6 +6,17 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GameMaster : MonoBehaviour
+{
+    [SerializeField] private Slider healthbarEnemy;
+    [SerializeField] private Slider healthbarPlayer;
+    [SerializeField] private CombatConfig Enemy;
+    [SerializeField] private CombatConfig Player;
+
+
     void Start()
     {
+        healthbarEnemy.value = 1;
+        healthbarPlayer.value = 1;
     }
+    }
+}
